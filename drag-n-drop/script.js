88f0464f5c2ec6c9b3e25c1b0c6165be1d876c70
Jakub Buśkiewicz <jakubbuskiewicz@gmail.com>
2022-02-24 15:47:50 +0100

    dnd final

---
+++ drag-n-drop/script.js
@@ -0,0 +1,55 @@
+// dragstart
+// dragend
+
+// dragover
+// dragenter
+// dragleave
+// drop
+
+const draggableEl = document.querySelector(".fill");
+const containersEl = document.querySelectorAll(".empty");
+
+draggableEl.addEventListener("dragstart", dragStart);
+draggableEl.addEventListener("dragend", dragEnd);
+
+const appState = {
+  items: [],
+};
+
+for (const container of containersEl) {
+  container.addEventListener("dragover", dragOver);
+  container.addEventListener("dragenter", dragEnter);
+  container.addEventListener("dragleave", dragLeave);
+  container.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  this.className += " hold";
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+  console.log(this.dataset);
+  this.className = "empty";
+  this.append(draggableEl);
+
+  console.log(draggableEl.parentElement.dataset);
+
+  const request = JSON.stringify(appState.items);
+}
