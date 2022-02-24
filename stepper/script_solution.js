275e70449a6465f5ea5085e4a72cf7f730f08d00
Smugowski <lukasz.smugowski@accenture.com>
2022-02-24 13:20:00 +0100

    init progress

---
+++ stepper/script_solution.js
@@ -0,0 +1,19 @@
+//wybieramy progress, buttony i circles
+
+let currentActiveStep = 1;
+let StepCount = 4;
+/*
+$(document).ready(() => {
+    const element = document.getElementById("next");
+    element.onclick = function changeWidth(){
+        var e1 = document.getElementById("next");
+        e1.style.width = "400px";
+    } ;
+  });
+*/
+
+/*document.getElementById('next').click(() => {
+    //currentActiveStep = 1+1;
+    //let currentProgress = 100 * (4 - 1);
+    //$('.progress').attr(width, currentProgress);
+  });*/
