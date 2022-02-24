6dd12f22dca8d842600f32734d791e218666af30
Smugowski <lukasz.smugowski@accenture.com>
2022-02-24 18:46:12 +0100

    stepper final

--- stepper/script_solution.js
+++ stepper/script_solution.js
@@ -1,19 +1,53 @@
 //wybieramy progress, buttony i circles
 
-let currentActiveStep = 1;
-let StepCount = 4;
-/*
-$(document).ready(() => {
-    const element = document.getElementById("next");
-    element.onclick = function changeWidth(){
-        var e1 = document.getElementById("next");
-        e1.style.width = "400px";
-    } ;
-  });
-*/
+const btnnext = document.getElementById("next");
+const btnprev = document.getElementById("prev");
+const progressbar = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+var stepCount = circles.length;
+var stepIndex = 1;
 
-/*document.getElementById('next').click(() => {
-    //currentActiveStep = 1+1;
-    //let currentProgress = 100 * (4 - 1);
-    //$('.progress').attr(width, currentProgress);
-  });*/
+btnnext.addEventListener("click",(e)=> {
+  modifyStepIndex(1);
+  modifyProgressBar();
+  disableEnableButtons();
+}
+);
+
+btnprev.addEventListener("click",(e)=> {
+  modifyStepIndex(-1);
+  modifyProgressBar();
+  disableEnableButtons();
+}
+);
+
+function modifyStepIndex(v){
+  stepIndex += v; 
+}
+
+function modifyProgressBar(){
+  circles.forEach((circle, idx) => {
+    if (idx < stepIndex) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  let newwidth = ((actives.length - 1) / (stepCount - 1)) * 100 + "%";
+  progressbar.style.width = newwidth;
+}
+
+function disableEnableButtons(){
+  if (stepIndex == 1) {
+    prev.disabled = true;
+  } else if (stepIndex == stepCount) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+
+}
