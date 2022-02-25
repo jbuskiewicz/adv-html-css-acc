f8d8e69ec6f656a12537abc1358a4d0d6acf70a6
Jakub Buśkiewicz <jakubbuskiewicz@gmail.com>
2022-02-25 09:43:52 +0100

    xhr request - part 1

---
+++ movie/notes.js
@@ -0,0 +1,27 @@
+// const person = {
+//   name: "Kuba",
+//   city: "Poznan",
+// };
+
+// const person2 = Object.create(person);
+
+// Object.defineProperty(person, "hello", {
+//   writable: false,
+//   enumerable: false,
+//   configurable: false,
+// });
+
+// for (const key in person) {
+//   console.log(key);
+// }
+
+// person2.name = "Jasiu";
+// delete person2.name;
+
+// -------
+// GET
+// xhr.send();
+// xhr.send(null);
+
+// POST
+// xhr.send(formData);
