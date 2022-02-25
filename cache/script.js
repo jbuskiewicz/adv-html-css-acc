08740089eed2dd29aa33f4319ee42695fda92509
Jakub Buśkiewicz <jakubbuskiewicz@gmail.com>
2022-02-25 14:01:53 +0100

    caches

--- cache/script.js
+++ cache/script.js
@@ -1,15 +1,26 @@
+// 'caches' in self
 (async () => {
   const cache = await caches.open("myCache");
-  console.log(cache);
+  //   console.log(cache);
 
-  const req = new Request("https://jsonplaceholder.typicode.com/users/1");
+  let req = new Request("https://jsonplaceholder.typicode.com/users/1");
+  cache.add(req);
 
-  fetch(req)
-    .then((data) => data.json())
-    .then(console.log);
+  if (await cache.match(req)) {
+    const data = await cache.match(req);
+    const json = await data.json();
+    console.log(json);
+    // printData(json)
+    return;
+  } else {
+    req = new Request("https://jsonplaceholder.typicode.com/users/1");
+  }
 })();
 
 // dodajemy przykladowy request = new Request(url)
 // cache.add(req)
 
 // cache.match(req) => zapisujemy wynik do zmiennej (await)
+const printData = (element) => {
+  document.appendChild;
+};
